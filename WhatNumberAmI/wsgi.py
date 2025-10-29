7822e1255cb880cb193ba2c6cbf497563235b9ea
Austin Woods <austinwoods99@gmail.com>
2025-10-29 16:01:26 -0500

    Initial Commit

---
+++ WhatNumberAmI/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for WhatNumberAmI project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatNumberAmI.settings')
+
+application = get_wsgi_application()
