c8707851bf0c80ce96eab98266dbc8ea09b04ac6
Austin Woods <austinwoods99@gmail.com>
2025-10-30 18:28:15 -0500

    Set up UserImage Model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
- 
+image_storage = "/Users/austinwoods/CodingProjects/Python/DumpFiles"
+
+
+class UserImage(models.Model):
+    userImage = models.ImageField(upload_to=image_storage)
+    userAnswer = models.CharField(max_length=30)
+        
